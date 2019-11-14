5790f3dd47ef35916327426df83f1df88d577fd7
roshangurung <r.gurung1992@gmail.com>
2019-11-14 12:26:56 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Working_with_Directories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.CreateDirectory(@"c:\temp\folder1");
+
+            var files = Directory.GetFiles(@"C:\Program Files", "*.*", SearchOption.AllDirectories);
+            foreach(var file in files)
+                Console.WriteLine(file);
+            
+
+        }
+    }
+}
